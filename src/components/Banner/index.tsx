ed11d0dffed831b8777b500b2f6f9d2003321b78
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2024-08-04 21:15:58 -0300

    feat: create banner design

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -4,10 +4,10 @@ export default function Banner() {
         <header className='w-full h-[420px] bg-zinc-400 bg-home bg-cover bg-center'>
             <div className='w-full h-full flex flex-col items-center justify-center'>
                 <img src={Burger} className="w-32 h-32 shadow-lg rounded-full hover:scale-110 duration-200" alt='logo-burger'/>
-                <h1 className='text-4xl mt-4 mb-2 font-bold text-white'>Dev burguer</h1>
+                <h1 className='text-4xl mt-4 mb-2 font-bold text-white'>Dev Burguer</h1>
                 <span className='text-white font-medium'>Rua Rogéria 37, Parque Ulisses, Rio de janeiro - RJ</span>
-                <div className='bg-amber-500 px-4 py-1 rounded-lg'>
-                    <span>De Seg a Dom - 18:00 as 22:00 </span>
+                <div className='bg-amber-500 px-4 py-1 rounded-lg mt-5'>
+                    <span className='text-white'>De Seg a Dom - 18:00 as 22:00 </span>
                 </div>
             </div>
         </header>
